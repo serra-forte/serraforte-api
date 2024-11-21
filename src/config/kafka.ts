def8b0aff01b44f2511e6bfbcd91e8652e53131b
Kaio Moreira <kaio-dev@outlook.com>
2024-11-21 18:51:25 -0300

    0.19.0 connect for kafka producer for new connection

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -2,7 +2,6 @@ import { Kafka } from "kafkajs";
 
 export const kafka =  new Kafka({
     clientId: "serraforte",
-    brokers: [`https://kafka-broker-production-37ae.up.railway.app:9092`], // teste http://localhost:9092
-    ssl: true,
+    brokers: [`https://kafka-broker-production-09a9.up.railway.app:9092`], // teste http://localhost:9092
 });
 

51c4caa88d908bba30c58eb712b5f6f5035381a9
Kaio Moreira <kaio-dev@outlook.com>
2024-11-21 17:15:45 -0300

    0.15.3 create connection for kafka

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -2,7 +2,8 @@ import { Kafka } from "kafkajs";
 
 export const kafka =  new Kafka({
     clientId: "serraforte",
-    brokers: ["localhost:9092"],
-
+    brokers: process.env.NODE_ENV === 'production' 
+    ? ['kafka-service:9092']  // para produção no Railway
+    : ['localhost:9092'],      // para desenvolvimento local
 });
 

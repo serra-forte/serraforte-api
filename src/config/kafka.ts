74c0bb07329dc89676e7bd35d2e69aeddf4b1b13
Kaio Moreira <kaio-dev@outlook.com>
2024-11-21 18:28:37 -0300

    0.17.0 create env for public conection kafka

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,7 +1,8 @@
+import { env } from "@/env";
 import { Kafka } from "kafkajs";
 
 export const kafka =  new Kafka({
     clientId: "serraforte",
-    brokers: ["localhost:9092"], // teste http://localhost:9092
+    brokers: [`${env.KAFKA_PUBLIC_URL}:9092`], // teste http://localhost:9092
 });
 

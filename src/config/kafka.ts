8b415a296020be1327e8e1ae4f153d52084a1524
Kaio Moreira <kaio-dev@outlook.com>
2024-11-21 18:14:57 -0300

    0.15.5 start for test connection for kafka

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -2,6 +2,6 @@ import { Kafka } from "kafkajs";
 
 export const kafka =  new Kafka({
     clientId: "serraforte",
-    brokers: ["localhost:9092"],
+    brokers: ["localhost:9092"], // teste http://localhost:9092
 });
 

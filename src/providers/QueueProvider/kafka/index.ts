35f3d0c3146362b3e81a2cfeffce782c29ce7177
Kaio Moreira <kaio-dev@outlook.com>
2024-11-22 15:31:15 -0300

    0.22.4 create config kafka with producer and consumer teste

--- src/providers/QueueProvider/kafka/index.ts
+++ src/providers/QueueProvider/kafka/index.ts
@@ -4,7 +4,5 @@ import { Kafka } from 'kafkajs';
 // Criar a instância do Kafka com as configurações corretas
 export const kafka = new Kafka({
     clientId: "serraforte", // Identificador do cliente
-    brokers: env.NODE_ENV === "development"
-        ? ["localhost:9092"] // IPv4 local
-        : [`[${env.KAFKA_PRIVATE_URL}]:29092`] // Endereço IPv6 com porta
+    brokers: env.NODE_ENV === "development" ? ["localhost:9092"] : [env.KAFKA_PRIVATE_URL], // Broker interno (utilize o nome do host adequado)
 });

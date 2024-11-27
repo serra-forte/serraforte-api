a6621c0ed489d098b18b89e5af1c5ee018423a12
Kaio Moreira <kaio-dev@outlook.com>
2024-11-27 19:30:55 -0300

    add: log for see order in webhook

--- src/providers/QueueProvider/kafka/index.ts
+++ src/providers/QueueProvider/kafka/index.ts
@@ -5,5 +5,4 @@ import { Kafka, logLevel } from 'kafkajs';
 export const kafka = new Kafka({
     clientId: "serraforte", // Identificador do cliente
     brokers: env.NODE_ENV === "development" ? ["localhost:9092"] : [env.KAFKA_PRIVATE_URL], // Broker interno (utilize o nome do host adequado)
-    logLevel: logLevel.DEBUG,
 });

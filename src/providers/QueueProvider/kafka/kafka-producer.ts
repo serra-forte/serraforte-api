744f4ffa1e808f3eb4b518e99c00810c118e6a8c
Kaio Moreira <kaio-dev@outlook.com>
2024-12-09 19:11:03 -0300

    add: create pationer for legacy kafka

--- src/providers/QueueProvider/kafka/kafka-producer.ts
+++ src/providers/QueueProvider/kafka/kafka-producer.ts
@@ -1,9 +1,11 @@
 import { kafka } from "@/providers/QueueProvider/kafka";
+import { Partitioners } from "kafkajs";
 
 export class KafkaProducer {
     async execute(topic: string, paylod: any) {
         const producer = kafka.producer({
-            allowAutoTopicCreation: true
+            allowAutoTopicCreation: true,
+            createPartitioner: Partitioners.LegacyPartitioner
         })
     
         await producer.connect()

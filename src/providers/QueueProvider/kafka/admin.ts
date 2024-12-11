9b8643c21878d53a1bbd2c8b9d2f308b7a204907
Kaio Moreira <kaio-dev@outlook.com>
2024-12-11 18:03:21 -0300

    create: consumer for generate label link for melhor envio

---
+++ src/providers/QueueProvider/kafka/admin.ts
@@ -0,0 +1,29 @@
+import { Admin } from "kafkajs";
+import { kafka } from ".";
+
+export class KafkaAdmin{
+    admin: Admin
+    constructor(){
+        this.admin = kafka.admin()
+    }
+
+    async connect() {
+        await this.admin.connect();
+      }
+    
+      async disconnect() {
+        await this.admin.disconnect();
+      }
+    
+      async createTopics(topic: string) {
+        await this.admin.createTopics({ 
+            topics: [
+                {
+                topic,
+                numPartitions: 3,
+                replicationFactor: 1
+            }]
+         });
+        console.log('Tópicos criados com sucesso!');
+      }
+}

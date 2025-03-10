b041954c0c7de9e567482acb6e2998e1571c85f9
Kaio Moreira <kaio-dev@outlook.com>
2025-03-10 19:22:56 -0300

    fix: route for send verification email

--- src/providers/MailProvider/implementations/provider-sendgrid.ts
+++ src/providers/MailProvider/implementations/provider-sendgrid.ts
@@ -32,7 +32,7 @@ export class MailProvider implements IMailProvider{
 
             const msg = {
                 to: email, // Para 
-                from: 'serra.forte@maissi.com.brg', // De 
+                from: 'serra.forte@maissi.com.br', // De 
                 subject: subject, // Assunto
                 html: htmlTemplate,
               };

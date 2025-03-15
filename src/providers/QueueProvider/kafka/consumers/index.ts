8fa066f9a9777169d84f86b5289d8f7f262086fc
Kaio Moreira <kaio-dev@outlook.com>
2025-03-15 12:44:54 -0300

    create: new consumer for generated track ling

--- src/providers/QueueProvider/kafka/consumers/index.ts
+++ src/providers/QueueProvider/kafka/consumers/index.ts
@@ -2,3 +2,4 @@ import "../consumers/melhor-envio/add-freight-to-cart-melhor-envio";
 import "../consumers/melhor-envio/payment-process-in-cart-melhor-envio";
 import "./melhor-envio/generate-label-melhor-envio";
 import "./melhor-envio/generate-label-to-print-melhor-envio";
+import "./melhor-envio/generate-tracking-link-melhor-envio";

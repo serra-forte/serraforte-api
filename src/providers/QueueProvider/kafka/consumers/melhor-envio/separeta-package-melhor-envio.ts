21b525bb10f699e86ec363f9176e1dc1efb63ed9
Kaio Moreira <kaio-dev@outlook.com>
2025-03-29 14:38:23 -0300

    create: new consumer for separate packege label 1.0.4

--- src/providers/QueueProvider/kafka/consumers/melhor-envio/separeta-package-melhor-envio.ts
+++ src/providers/QueueProvider/kafka/consumers/melhor-envio/separeta-package-melhor-envio.ts
@@ -161,7 +161,7 @@ export class SeparatePackageMelhorEnvio {
                         packages.push(currentPackage);
                     }
                     
-                    await this.kafkaProducer.execute('ADD_FREIGHT_TO_CART', packages);
+                    await this.kafkaProducer.execute('ADD_FREIGHT_TO_CART', {packages});
                 } catch (error) {
                     console.error('[Consumer - Separate Package] Erro ao processar mensagem:', error);
                 }

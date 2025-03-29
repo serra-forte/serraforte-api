dfd6b54761f465deff83d7b783ecf94d95e75abf
Kaio Moreira <kaio-dev@outlook.com>
2025-03-29 16:34:58 -0300

    create: new consumer for separate packege label 1.1.5

--- src/providers/QueueProvider/kafka/consumers/melhor-envio/add-freight-to-cart-melhor-envio.ts
+++ src/providers/QueueProvider/kafka/consumers/melhor-envio/add-freight-to-cart-melhor-envio.ts
@@ -86,7 +86,6 @@ export class AddFreightToCartMelhorEnvio {
 
                         if(itemPackage.companyName === 'Correios') {
                             for(let item of itemPackage.items) {
-                                console.dir(item, { depth: 1 })
                                 const freightInCart = await this.melhorEnvioProvider.addFreightToCart({
                                     from: {
                                         name: shopkeeper.name,

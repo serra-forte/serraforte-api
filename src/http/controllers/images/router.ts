30c60515c236d8f1932eec73c46bc6e9c98ca553
Kaio Moreira <kaio-dev@outlook.com>
2025-04-05 16:13:03 -0300

    add: vlaidaiton for login send imagem

--- src/http/controllers/images/router.ts
+++ src/http/controllers/images/router.ts
@@ -7,7 +7,7 @@ import { ListImageByUser } from "./list-by-user/list-by-user-images-controller";
 import { ListImage } from "./list/list-images-controller";
 
 export async function imageRoutes(fastifyApp: FastifyInstance) {
-    // fastifyApp.addHook('onRequest', verifyTokenJWT)
+    fastifyApp.addHook('onRequest', verifyTokenJWT)
     // fastifyApp.addHook('onRequest', verifyUserRole('ADMIN', 'SUPER'))
     
     // upload de imagens

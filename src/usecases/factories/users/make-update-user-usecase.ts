1ce498b9a9675145f13081dc6ba7bc78f41e3796
Kaio Moreira <kaio-dev@outlook.com>
2025-04-13 14:36:26 -0300

    fix: update user with address 1.0.1

--- src/usecases/factories/users/make-update-user-usecase.ts
+++ src/usecases/factories/users/make-update-user-usecase.ts
@@ -1,9 +1,11 @@
+import { PrismaAddressesRepository } from "@/repositories/prisma/prisma-addresses-repository";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UpdateUserUseCase } from "@/usecases/users/update-full/update-user-usecase";
 
 export async function makeUpdateUser(): Promise<UpdateUserUseCase> {
     const usersRepository = new PrismaUsersRepository();
-    const updateUserUseCase = new UpdateUserUseCase(usersRepository)
+     const addressRepository = new PrismaAddressesRepository();
+    const updateUserUseCase = new UpdateUserUseCase(usersRepository, addressRepository)
 
     return updateUserUseCase
 }

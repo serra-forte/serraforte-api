1087fac203b7d1fbdac3ce5962d5195156512754
Kaio Moreira <kaio-dev@outlook.com>
2025-05-10 18:10:50 -0300

    feat: create order to bier held 1.0.0

--- src/providers/QueueProvider/kafka/consumers/index.ts
+++ src/providers/QueueProvider/kafka/consumers/index.ts
@@ -3,3 +3,4 @@ import "./implementations/melhor-envio/payment-process-in-cart-melhor-envio";
 import "./implementations/melhor-envio/generate-label-melhor-envio";
 import "./implementations/melhor-envio/generate-label-to-print-melhor-envio";
 import "./implementations/melhor-envio/separeta-package-melhor-envio";
+import "./implementations/bierl-held/create-order-bier-held";

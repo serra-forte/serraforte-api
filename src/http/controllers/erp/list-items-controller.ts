d45eb99fb0f3b5c6e687783f40fcdc9f340ca853
Kaio Moreira <kaio-dev@outlook.com>
2025-05-12 16:38:30 -0300

    feat: create route for list items erp 1.0.1

--- src/http/controllers/erp/list-items-controller.ts
+++ src/http/controllers/erp/list-items-controller.ts
@@ -7,23 +7,23 @@ export async function ListItems(request: FastifyRequest, reply:FastifyReply){
         const itemsParams = z.object({
                 clientId: z.number().nonnegative(),
                 page: z.coerce.number().optional().default(1),
-                per_page: z.coerce.number().optional().default(10),
+                perPage: z.coerce.number().optional().default(10),
                 filterSearch: z.string().optional(),
             })
 
         const { 
             clientId,
             page,
-            per_page,
+            perPage,
             filterSearch
-        } = itemsParams.parse(request.params)
+        } = itemsParams.parse(request.query)
         
         const listItemUseCase = await makeListItems()
         
         const listItems = await listItemUseCase.execute({
             client_id: clientId,
             page,
-            per_page,
+            per_page: perPage,
             search: filterSearch
         })
         

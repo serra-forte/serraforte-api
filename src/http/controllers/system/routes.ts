320b7f4abcc78af99eff70be5185570a3ed1c7e0
Kaio Moreira <kaio-dev@outlook.com>
2025-05-13 14:55:26 -0300

    fix: system status return 1.0.2

--- src/http/controllers/system/routes.ts
+++ src/http/controllers/system/routes.ts
@@ -4,8 +4,5 @@ import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 import { IsSystemUpdating } from "./is-system-updating-controller";
 
 export async function systemRoutes(fastifyApp: FastifyInstance) {
-   fastifyApp.addHook('onRequest', verifyTokenJWT)
-   fastifyApp.addHook('onRequest', verifyUserRole('ADMIN', 'SUPER'))
-
    fastifyApp.get('/status', IsSystemUpdating)
 }

f1c99af488e5b590ab385176226f415a40dc72b1
Kaio Moreira <kaio-dev@outlook.com>
2025-06-01 16:09:24 -0300

    fix: update user if has erp 1.0.1

--- src/usecases/factories/users/make-update-user-usecase.ts
+++ src/usecases/factories/users/make-update-user-usecase.ts
@@ -1,4 +1,5 @@
 import { BierHeldProvider } from "@/providers/BierHeldProvider/implementations/bier-held-provider";
+import { RemoteConfigProviderFirebase } from "@/providers/RemoteConfigProvider/implementations/provider-remote-config";
 import { PrismaAddressesRepository } from "@/repositories/prisma/prisma-addresses-repository";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { UpdateUserUseCase } from "@/usecases/users/update-full/update-user-usecase";
@@ -7,7 +8,8 @@ export async function makeUpdateUser(): Promise<UpdateUserUseCase> {
     const usersRepository = new PrismaUsersRepository();
      const addressRepository = new PrismaAddressesRepository();
      const bierHeldProvider = new BierHeldProvider();
-    const updateUserUseCase = new UpdateUserUseCase(usersRepository, addressRepository, bierHeldProvider)
+     const remoteConfig = new RemoteConfigProviderFirebase()
+    const updateUserUseCase = new UpdateUserUseCase(usersRepository, addressRepository, bierHeldProvider,remoteConfig)
 
     return updateUserUseCase
 }

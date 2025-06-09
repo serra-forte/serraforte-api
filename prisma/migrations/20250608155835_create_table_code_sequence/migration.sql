33a2d7e41ddb1bb31673c6682b848f95d4d48a1a
Kaio Moreira <kaio-dev@outlook.com>
2025-06-09 07:43:21 -0300

    feat: remake create order 1.0.2

--- prisma/migrations/20250608155835_create_table_code_sequence/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "create_code_sequence" (
-    "id" TEXT NOT NULL,
-    "last_number" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "create_code_sequence_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20250608162005_alter_order_code_increment/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE orders_code_seq;
-ALTER TABLE "orders" ALTER COLUMN "code" SET DEFAULT nextval('orders_code_seq');
-ALTER SEQUENCE orders_code_seq OWNED BY "orders"."code";

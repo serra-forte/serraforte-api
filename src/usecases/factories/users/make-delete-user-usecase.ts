d168feca02025e22c032e5ee67faaebea297955b
Kaio Moreira <kaio-dev@outlook.com>
2025-06-21 08:28:51 -0300

    fix: delete user 1.0.1

--- src/usecases/factories/users/make-delete-user-usecase.ts
+++ src/usecases/factories/users/make-delete-user-usecase.ts
@@ -1,10 +1,12 @@
+import { PrismaOrderRepository } from "@/repositories/prisma/prisma-orders-repository";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { DeleteUserUseCase } from "@/usecases/users/delete/delete-user-usecase";
 
 export async function makeDeleteUser(): Promise<DeleteUserUseCase> {
     const usersRepository = new PrismaUsersRepository();
+    const orderRepository = new PrismaOrderRepository()
 
-    const deleteUserUseCase = new DeleteUserUseCase(usersRepository)
+    const deleteUserUseCase = new DeleteUserUseCase(usersRepository, orderRepository)
 
     return deleteUserUseCase
 }

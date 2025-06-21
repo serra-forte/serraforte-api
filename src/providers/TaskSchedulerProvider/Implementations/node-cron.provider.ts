6fa77d469d575dee45678c747f3d2dd19ce2621e
Kaio Moreira <kaio-dev@outlook.com>
2025-06-21 10:08:18 -0300

    test: job for update order expired 1.0.5

--- src/providers/TaskSchedulerProvider/Implementations/node-cron.provider.ts
+++ src/providers/TaskSchedulerProvider/Implementations/node-cron.provider.ts
@@ -120,14 +120,16 @@ export class NodeCronProvider implements INodeCronProvider {
                 // buscar pedidos sem pagamento a 24 horas
                 const orders = await this.orderRepository.listByPaymentWithoutPaying24Hours();
 
-                // const listDeliveryMan = await this.userRepository.listByDeliveryMan(1, 1)
-
                 // [x] buscar todos os usuarios administradores
                 const listUsersAdmin = await this.userRepository.listAdmins()
 
-                // validar se o entregador existe
-                // const uniqueDeliveryMan = listDeliveryMan.users[0] as User
-                
+                // pegar shopkeepers pelo item do pedido
+                const findShopkeeper = await this.userRepository.findShopkeeper()
+
+                if(!findShopkeeper){
+                    throw new AppError('Lojista nao encontrado', 404)
+                }
+
                 // for para percorrer os pedidos
                 for(let order of orders){
                     // criar data limite em utc para comparar com a data de criação do pedido
@@ -136,100 +138,63 @@ export class NodeCronProvider implements INodeCronProvider {
                     // validar se a data de criação ultrapassou a data limite de pagamento
                     const checkLimitDateToCancel = this.dayjsProvider.compareIfAfter(getLimitDateToPayment, dateNow)
 
-                    console.log(checkLimitDateToCancel)
                     // validar se a data de criação ultrapassou a data limite de pagamento
                     if(!checkLimitDateToCancel){
-                        console.log('log 1')
                          // for para percorrer os itens de cada pedido
                         for(let item of order.items){
-                            const endOrder: IOrderRelationsDTO = {
-                                user: order.user,
-                                delivery: {
-                                    address: order.delivery.address ? order.delivery.address : undefined
-                                },
-                                payment: orders[0].payment,
-                                total: 0, // Inicializa total como 0
-                                items: [] // Inicializa items como array vazio
-                              } as unknown as IOrderRelationsDTO;
-
-                            // let existDeliveryMan = false
-                            // let listShopkeeper = []
-                            let total = Number(order.total);  // Certifica que 'total' é um número
-                            endOrder.total += total;          // Acumula o total
-                            endOrder.items.push(...order.items); // spreed no array de items para acumular os items anteriores e os novos
-                    
-                            // pegar shopkeepers pelo item do pedido
-                            const findShopkeeper = await this.userRepository.findShopkeeper()
-
-                            if(!findShopkeeper){
-                                throw new AppError('Lojista nao encontrado', 404)
-                            }
-
-                            // // validar se o shopkeeper existe
-                            // if(findShopkeeper){
-                            //     listShopkeeper.push(findShopkeeper)
-                            // }
-
-                            // if(!order.withdrawStore){
-                            //     existDeliveryMan = true
-                            // }
-                            
-                            // if(existDeliveryMan){
-                            //     endOrder.total += Number(uniqueDeliveryMan.paymentFee)
-                            // }
-
                             const quantity = Number(item.quantity)
 
                             // atualizar estoque com a quantidade de itens selecionado pelo usuário
                             this.productRepository.incrementQuantity(item.productId as string, quantity);
-
-                            // atualizar o status do pedido para expirado
-                            await this.orderRepository.updateStatus(order.id, Status.EXPIRED);
-
-                            // atualizar o status do pagamento para expirado
-                            await this.paymentRepository.updateStatus(order.payment.id, Status.EXPIRED);
-
-                            // cancelar fatura na asaas
-                            await this.asaasProvider.cancelPayment(order.payment.asaasPaymentId as string)
-
-                            // [x] adicioanr descrição no pedido para informar que o pagamento foi reprovado
-                            await this.orderRepository.addDescription(order.id, 'Reprovado por falta de pagamento') 
-
-
-                            // [x] criar variavel com caminho do templeate de email de pagamento reprovado
-                            const templatePathUserReproved =
-                            './views/emails/admin-payment-reproved.hbs'
-
-                            // [x] disparar envio de email de pagamento recebido do usuário com nota fiscal(invoice)
-                            await this.mailProvider.sendEmail(
-                                order.user.email,
-                                order.user.name,
-                                'Pagamento Expirado',
-                                order.payment.invoiceUrl,
-                                templatePathUserReproved,
-                                {
-                                order: endOrder,
-                                },
-                            )
-
-
-                            // ENVIAR EMAIL PARA ADMIN DO SISTEMA AVISANDO QUE O PAGAMENTO FOI REPROVADO
-                            // [x] for para buscar users administradores e enviar email de pagamento reprovado
-                            for (const admin of listUsersAdmin) {
-                                await this.mailProvider.sendEmail(
-                                admin.email,
-                                admin.name,
-                                `Pagamento Expirado`,
-                                order.payment.invoiceUrl,
-                                templatePathUserReproved,
-                                {
-                                    order: endOrder,
-                                },
-                                )
-                            }
-         
                         }
                     }
+
+                    // atualizar o status do pedido para expirado
+                    await this.orderRepository.updateStatus(order.id, Status.EXPIRED);
+
+                    // atualizar o status do pagamento para expirado
+                    await this.paymentRepository.updateStatus(order.payment.id, Status.EXPIRED);
+
+                    // cancelar fatura na asaas
+                    await this.asaasProvider.cancelPayment(order.payment.asaasPaymentId as string)
+
+                    // [x] adicioanr descrição no pedido para informar que o pagamento foi reprovado
+                    await this.orderRepository.addDescription(order.id, 'Reprovado por falta de pagamento') 
+
+
+                    // [x] criar variavel com caminho do templeate de email de pagamento reprovado
+                    const templatePathUserReproved =
+                    './views/emails/admin-payment-reproved.hbs'
+
+                    // [x] disparar envio de email de pagamento recebido do usuário com nota fiscal(invoice)
+                    await this.mailProvider.sendEmail(
+                        order.user.email,
+                        order.user.name,
+                        'Pagamento Expirado',
+                        order.payment.invoiceUrl,
+                        templatePathUserReproved,
+                        {
+                            order,
+                        },
+                    )
+
+
+                    // ENVIAR EMAIL PARA ADMIN DO SISTEMA AVISANDO QUE O PAGAMENTO FOI REPROVADO
+                    // [x] for para buscar users administradores e enviar email de pagamento reprovado
+                    for (const admin of listUsersAdmin) {
+                        await this.mailProvider.sendEmail(
+                        admin.email,
+                        admin.name,
+                        `Pagamento Expirado`,
+                        order.payment.invoiceUrl,
+                        templatePathUserReproved,
+                        {
+                            order,
+                        },
+                        )
+                    }
+
+                    console.log('[Cron Job - CheckPayment24Hours]Pagamento expirado com sucesso')
                 }
                
             } catch (error) {

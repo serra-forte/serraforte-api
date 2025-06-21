1f068462e13a6728044e291d34bedb0612ae4e32
Kaio Moreira <kaio-dev@outlook.com>
2025-06-21 09:02:42 -0300

    fix: delete all cart-items 1.0.0

--- src/usecases/factories/cart-items/make-delete-all-cart-items-usecase copy.ts
+++ src/usecases/factories/cart-items/make-delete-all-cart-items-usecase copy.ts
@@ -1,3 +1,4 @@
+import { RemoteConfigProviderFirebase } from "@/providers/RemoteConfigProvider/implementations/provider-remote-config"
 import { PrismaCartItemRepository } from "@/repositories/prisma/prisma-cart-item-repository"
 import { PrismaShoppingCartRepository } from "@/repositories/prisma/prisma-shopping-cart-repository"
 import { DeleteAllCartItemUseCase } from "@/usecases/cart-items/delete-all/delete-all-cart-item-usecase"
@@ -5,10 +6,12 @@ import { DeleteAllCartItemUseCase } from "@/usecases/cart-items/delete-all/delet
 export async function makeDeleteAllCartItem(): Promise<DeleteAllCartItemUseCase> {
     const cartItemsRepository = new PrismaCartItemRepository()
     const shoppingCartsRepository = new PrismaShoppingCartRepository()
+    const remoteConfigProvider = new RemoteConfigProviderFirebase()
 
     const deleteAllCartItemUseCase = new DeleteAllCartItemUseCase(
         shoppingCartsRepository,
         cartItemsRepository,
+        remoteConfigProvider
     )
 
     return deleteAllCartItemUseCase
